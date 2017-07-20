567ae0f3000128feb6569ee3909e38bdca5824dd
Greg Izzy <izydorek.g@gmail.com>
2017-07-20 19:46:07 +0200

    Update1

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 function getTriangleArea(a, h) {
   if ((a >0) && (h > 0)) {
     return a*h/2;
-    console.log(a*h/2)
+    //console.log(a*h/2)
   }
   if ((a <=0) && (h <= 0)) {
   console.log("nieprawidłowe dane");
@@ -15,5 +15,3 @@ console.log( getTriangleArea(10,6) )
 var triangle1Area = getTriangleArea(3, 15);
 var triangle2Area = getTriangleArea(7, 15);
 var triangle3Area = getTriangleArea(0, 15);
-
-//console.log(triangle1Area, triangle2Area, triangle3Area);
